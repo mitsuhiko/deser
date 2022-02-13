ac635a53d72e92b04b9619da5aa37b42ada137aa
Armin Ronacher <armin.ronacher@active-4.com>
2022-02-13 12:10:05 +0100

    Expanded docs on deser_json slightly

--- deser-json/src/lib.rs
+++ deser-json/src/lib.rs
@@ -1,6 +1,13 @@
 //! Parse and serialize JSON compatible with deser.
 //!
-//! This library is very bare bones at this point and not at all optimized.
+//! This library is very bare bones at this point and not at all optimized.  It is
+//! based on microserde which in turn is based on miniserde to achieve the most
+//! minimal implementation of a serializer and serializer.
+//!
+//! ```rust
+//! let vec: Vec<u64> = deser_json::from_str("[1, 2, 3, 4]").unwrap();
+//! let json = deser_json::to_string(&vec).unwrap();
+//! ```
 mod de;
 mod ser;
 
